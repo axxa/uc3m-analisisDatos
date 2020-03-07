3894f14c6891149dc2812483b44c52feed43d5de
Alvaro <asan123456@gmail.com>
2020-03-07 14:06:42 +0100

    feat: fix practica1

--- practica1/practica1Solucion.R
+++ practica1/practica1Solucion.R
@@ -1,4 +1,4 @@
-source("init.R")
+source("../lib/init.R")
 
 dax <- stockGetdata("DAX")
 dax.df <- stockSubset(dax, "2011-01-01", yahoo.lastday())
@@ -11,19 +11,33 @@ timestres <- c("Q1","Q2","Q3","Q4")
 strQs <- ifelse(numMonth <= 3, "Q1", ifelse(numMonth<=6,"Q2", ifelse(numMonth<=9,"Q3","Q4")))
 daxTrimestres <- factor(strQs)
 
+rent10 <- backReturns(dax.df$Adj.Close, 10)
+rent20 <- backReturns(dax.df$Adj.Close, 20)
+rent30 <- backReturns(dax.df$Adj.Close, 30)
+sma10 <- SMA_dev(dax.df$Adj.Close, 10)
+sma20 <- SMA_dev(dax.df$Adj.Close, 20)
+sma30 <- SMA_dev(dax.df$Adj.Close, 30)
+
 #Features Dataset
-daxFeatures <- data.frame(fecha = dax.df$Date,
+daxFeatures <- data.frame(
                           trimes = daxTrimestres,
-                          rent10 = backReturns(dax.df$Adj.Close, 10),
-                          rent20 = backReturns(dax.df$Adj.Close, 20),
-                          rent30 = backReturns(dax.df$Adj.Close, 30),
-                          sma10 = SMA_dev(dax.df$Adj.Close, 10),
-                          sma20 = SMA_dev(dax.df$Adj.Close, 20),
-                          sma30 = SMA_dev(dax.df$Adj.Close, 30),
+                          rent10 = rent10,
+                          rent20 = rent20,
+                          rent30 = rent30,
+                          sma10 = sma10,
+                          sma20 = sma20,
+                          sma30 = sma30,
                           daxY = daxY30)
 
-
+head(daxFeatures)
 daxExamples <- na.omit(daxFeatures)
+
+rentCorr <- data.frame(rent10 = rent10,sma10 = sma10,
+                        rent20 = rent20,sma20 = sma20,
+                        rent30 = rent30,sma30 = sma30
+                      )
+
+pairs(rentCorr,col = c("red","blue"))
 
 
 write.csv(daxExamples, "data/practica1Resultados.csv", row.names=FALSE)
